b96db8bb633608928342b16f1912785615531e72
dmpliamplias <dyonisos.mpliamplias@students.fhnw.ch>
2018-12-02 19:23:43 +0100

    Add on click behavior for table row

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -17,22 +17,18 @@
                           <th scope="col">Kohlenhydrate</th>
                           <th scope="col">Fett</th>
                           <th scope="col">Salz</th>
-                          <th scope="col">Bearbeiten</th>
                           <th scope="col">Erfasst am</th>
                           <th scope="col">Geändert am</th>
                         </tr>
                       </thead>
                       <tbody>
                         @foreach($products as $product)
-                        <tr>
+                        <tr onclick="window.location = '/product/{{ $product->id }}'">
                           <td>{{$product->name}}</td>
                           <td>{{$product->calorie_amount}} kcal</td>
                           <td>{{$product->carb_amount}} g</td>
                           <td>{{$product->fat_amount}} g</td>
                           <td>{{$product->salt_amount}} g</td>
-                          <td>
-                            <a href="{{ route('product.show', $product->id) }}}" class="btn btn-outline-primary" role="button" aria-pressed="true">Bearbeiten</a>
-                          </td>
                           <td>{{$product->created_at}}</td>
                           <td>{{$product->updated_at}}</td>
                         </tr>
