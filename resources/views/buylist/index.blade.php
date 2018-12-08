f8d926eacac29235de618a416e383b0d134df3f7
nicowaelti <44291459+nicowaelti@users.noreply.github.com>
2018-12-08 17:14:23 +0100

    PDF statisch parat

--- resources/views/buylist/index.blade.php
+++ resources/views/buylist/index.blade.php
@@ -1,4 +1,32 @@
-
+<html>
+  <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body>
     <div class="container">
-        <h1>Einkaufszettel</h1>
-    </div>
+      <h2>Einkaufszettel</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th style="width: 5%">Menge</th>
+              <th style="width: 10%">Einheit</th>
+              <th style="width: 60%">Artikel</th>
+              <th style="width: 20%">Preis</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1500</th>
+              <td>Gramm</td>
+              <td>Meerschweinchenhackfleisch</td>
+              <td>12.35</td>
+            </tr>
+          </tbody>
+        </table>
+
+      <p> Total Preis: 24.45</p>
+
+        </div>
+
+  </body>
+</html>
