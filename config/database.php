99875a79e23b15afc342731c16888705a7c52ad3
dmpliamplias <dyonisos.mpliamplias@students.fhnw.ch>
2018-12-08 12:53:14 +0100

    'Config'

--- config/database.php
+++ config/database.php
@@ -48,9 +48,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+//            'host' => env('DB_HOST', '127.0.0.1'),
             'host' => $host,
-//            'port' => env('DB_PORT', '3306'),
 //            'database' => env('DB_DATABASE', 'forge'),
             'database' => $database,
 //            'username' => env('DB_USERNAME', 'forge'),
