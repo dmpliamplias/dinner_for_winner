3348d66ad4276ebbd06e74b5f72edb8d43f16413
dmpliamplias <dyonisos.mpliamplias@students.fhnw.ch>
2018-12-10 00:29:13 +0100

    Try something for https

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('https://dinner4winner.herokuapp.com/', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
