050a3884764d0d1365ca770b9404345594a56000
nicowaelti <44291459+nicowaelti@users.noreply.github.com>
2018-12-09 19:36:57 +0100

    Kontaktseite und Mailtemplate erstellt

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h1>Dinner für Gewinner </h1>
+    <h2>Besten Dank für Ihre Nachricht!</h2>
+    </br>
+    <p>Wir werden Sie so schnell wie möglich kontaktieren</p></br>
+    <p>Sie haben folgende Nahchricht hinterlassen</p></br>
+    <h3>$subject</h3></br>
+    <p>$message</p>
+
+
+</div>
+</body>
+</html>
