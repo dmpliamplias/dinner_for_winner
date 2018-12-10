ff8af39bc0bfce078f1854b883b22b72e03971e6
nicowaelti <44291459+nicowaelti@users.noreply.github.com>
2018-12-10 19:25:11 +0100

    About optisch angepasst

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Über "Dinner für Gewinner"</h1>
-        <p> Dinner für Gewinner ist die ideale Plattform für alle, die:
-            <ul>
-                <li>Zeit sparen möchten</li>
-                <li>nicht wissen, was sie kochen sollen</li>
-                <li>es genau nehmen mit der Ernährung</li>
-                <li>die sich einfach gesund ernähren möchten</li>
-                <li>die genau aufs Budget schauen</li>
-                <li>Kalorien zählen</li>
-                <li>etwas Unterstützung beim Abnehmen gebrauchen können</li>
-                <li>nicht gerne einkaufen gehen</li>
-                <li>Lust haben unserere Webseite zu benützen</li>
-            </ul>
-            So schnell haben Sie noch nie einen Einkaufszettel geschrieben! Sparen Sie Zeit, die Sie fürs Kochen brauchen können.
-        </p>
+
+<div class="jumbotron">
+  <h1 class="display-4">Über "Dinner für Gewinner"</h1>
+  <p class="lead"> Dinner für Gewinner ist die ideale Plattform für alle, die:
+    <ul>
+        <li>Zeit sparen möchten</li>
+        <li>nicht wissen, was sie kochen sollen</li>
+        <li>es genau nehmen mit der Ernährung</li>
+        <li>die sich einfach gesund ernähren möchten</li>
+        <li>die genau aufs Budget schauen</li>
+        <li>Kalorien zählen</li>
+        <li>etwas Unterstützung beim Abnehmen gebrauchen können</li>
+        <li>nicht gerne einkaufen gehen</li>
+        <li>Lust haben unserere Webseite zu benützen</li>
+    </ul>
+
+  </p>
+  <hr class="my-4">
+  <p><strong>So schnell haben Sie noch nie einen Einkaufszettel geschrieben! Sparen Sie Zeit, die Sie fürs Kochen brauchen können.</strong></p>
+</div>
 
         <h2> Das Team </h2>
         <p>
